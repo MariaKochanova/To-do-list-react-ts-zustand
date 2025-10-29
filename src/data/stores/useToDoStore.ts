209bd96b9afcbcce53116c077be5d675bb539326
Mariya Kochanova <balalum@gmail.com>
2025-10-29 10:59:09 +0100

    Initial commit

---
+++ src/data/stores/useToDoStore.ts
@@ -0,0 +1,75 @@
+import create, { type State, type StateCreator } from 'zustand';
+
+import { generateId } from '../helpers';
+
+interface Task {
+    id: string;
+    title: string;
+    createdAt: number;
+}
+
+interface ToDoStore {
+    tasks: Task[];
+    createTask: (title: string) => void;
+    updateTask: (id: string, title: string) => void;
+    removeTask: (id: string) => void;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isToDoStore(object: any): object is ToDoStore {
+    return 'tasks' in object;
+}
+
+const localStorageUpdate = <T extends State>(config: StateCreator<T>):
+    StateCreator<T> => (set, get, api) => config((nextState, ...args) => {
+        console.log(nextState)
+        if (isToDoStore(nextState)) {
+            window.localStorage.setItem('tasks', JSON.stringify(
+                nextState.tasks
+            ))
+        }
+        set(nextState, ...args);
+    }, get, api);
+
+const getCurrentState = () => {
+    try {
+        const currentState = (JSON.parse(window.localStorage.getItem('tasks') || '[]')) as Task[];
+        return currentState
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+        window.localStorage.setItem('tasks', '[]')
+    }
+
+    return []
+}
+
+export const useToDoStore = create<ToDoStore>(localStorageUpdate((set, get) => ({
+    tasks: getCurrentState(),
+    createTask: (title) => {
+        const { tasks } = get();
+        const newTask = {
+            id: generateId(),
+            title,
+            createdAt: Date.now(),
+        }
+
+        set({
+            tasks: [newTask].concat(tasks),
+        })
+    },
+    updateTask: (id: string, title: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.map((task) => ({
+                ...task,
+                title: task.id === id ? title : task.title,
+            }))
+        })
+    },
+    removeTask: (id: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.filter((task) => task.id !== id)
+        })
+    },
+})))
---
+++ src/views/App/index.tsx
@@ -0,0 +1,42 @@
+import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+import { InputTask } from '../components/InputTask';
+
+import styles from './index.module.scss';
+
+export const App: React.FC = () => {
+    const tasks = useToDoStore(state => state.tasks);
+    const createTask = useToDoStore(state => state.createTask);
+    const updateTask = useToDoStore(state => state.updateTask);
+    const removeTask = useToDoStore(state => state.removeTask);
+
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            <section className={styles.articleSection}>
+                <InputPlus
+                    onAdd={(title) => {
+                        if (title) {
+                            createTask(title)
+                        }
+                    }}
+                />
+            </section>
+            <section className={styles.articleSection}>
+                {!tasks.length && (
+                    <p className={styles.articleText}>There is no one task.</p>
+                )}
+                {tasks.map((task) => (
+                    <InputTask
+                        key={task.id}
+                        id={task.id}
+                        title={task.title}
+                        onDone={removeTask}
+                        onEdited={updateTask}
+                        onRemoved={removeTask}
+                    />
+                ))}
+            </section>
+        </article >
+    )
+}
